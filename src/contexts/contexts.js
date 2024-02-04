d8eee6e813d86c5131daf5657faf99efe1f969ba
rhysw97 <rhysw97@gmail.com>
2024-02-04 18:44:45 +0000

    deploying latest version

--- src/contexts/contexts.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-export const USERNAME = createContext('')
-
-export const LOGGEDIN = createContext(false)
--- src/utils/data-utils.tsx
+++
@@ -1,9 +0,0 @@
-export function getInputData(inputList: Array<HTMLInputElement>) : Object {
-    const inputData = {}
-    inputList.forEach(input => {
-        inputData[input.id] = input.value
-        input.value = ''
-    })
-
-    return inputData
-}
